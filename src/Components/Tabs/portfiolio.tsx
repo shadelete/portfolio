6499487b6df6d564737ff58420dbdafb8b95cb39
shadelete <drgbkbeats@gmail.com>
2022-11-06 14:01:34 +0200

    add new demos

--- src/Components/Tabs/portfiolio.tsx
+++ src/Components/Tabs/portfiolio.tsx
@@ -10,14 +10,26 @@ const TabsPortfolio = () => {
 		{
 			name: 'EHB INTERNATIONAL HORSE TRANSPORT',
 			stack: 'WordPress',
-			urlImage: conImgUrl('ehb.jpg'),
+			urlImage: conImgUrl('ehb-min.webp'),
 			url: 'https://ehbinternational.co.uk/'
+		},
+		{
+			name: "IXART'S BLOG",
+			stack: 'React | TS | Tailwind',
+			urlImage: conImgUrl('ixarts-min.webp'),
+			url: 'https://demo-ixartz-blog.netlify.app/'
+		},
+		{
+			name: 'FINDTREND LANDING PAGE',
+			stack: 'SASS | Gulp',
+			urlImage: conImgUrl('findtrend-min.webp'),
+			url: 'https://demo-findtend.netlify.app/'
 		}
 	]
 
 	return (
 		<div className="py-5">
-			<div className="container container mx-auto 2xl max-w-2xl grid grid-cols-1 ">
+			<div className="container container mx-auto 2xl max-w-2xl grid grid-cols-1 gap-5">
 
 				{worksDB.map((el:any)=> {
 					return (
@@ -27,7 +39,7 @@ const TabsPortfolio = () => {
 								className="portfolio relative bg-cover min-h-[10rem] sm:min-h-[20rem] items-center justify-end cursor-pointer rounded-3xl flex flex-col"
 							>
 								<div className="portfolio-item">
-									<h1 className="text-prim">{el.name}</h1>
+									<h1 className="text-prim text-[.8rem] md:text-[1rem]">{el.name}</h1>
 									<h1>{el.stack}</h1>
 								</div>
 							</div>
